bbf68262acfb5cf9714e08d41d9b5a6a09fd8722
syuchan1005 <syuchan.dev@gmail.com>
2018-04-26 21:44:46 +0900

    :tada: v3.2 update

--- deemomusicorganizer/DeemoMusicOganizer.py
+++ deemomusicorganizer/DeemoMusicOganizer.py
@@ -1,8 +1,9 @@
 import os
+import sys
 import json
 import shutil
 from pydub import AudioSegment
-from mutagen.id3 import ID3, TIT2, TALB, TPE1, TRCK, APIC, TDRC, TCON
+from mutagen.id3 import ID3, TIT2, TALB, TPE1, TRCK, APIC, TCON, TCMP
 from mutagen.mp3 import MP3
 import mutagen.id3
 
@@ -12,7 +13,7 @@ def WAVtoMP3(path: str):
         AudioSegment.from_wav(path).export(path[:-3] + "mp3", format="mp3")
 
 
-def setMetaTag(filename, title, artist, album, track, track_album):
+def setMetaTag(filename, title, artist, album, track, track_album, pic_data):
     audio = MP3(filename, ID3=ID3)
     try:
         audio.add_tags(ID3=ID3)
@@ -22,8 +23,32 @@ def setMetaTag(filename, title, artist, album, track, track_album):
     audio["TPE1"] = TPE1(encoding=3, text=artist)
     audio["TALB"] = TALB(encoding=3, text=album)
     audio['TCON'] = TCON(encoding=3, text="Deemo")
-    audio["TRCK"] = TRCK(encoding=3, text=[(track, track_album)])
+    audio["TRCK"] = TRCK(encoding=3, text=str(track) + "/" + str(track_album))
+    audio["TCMP"] = TCMP(encoding=3, text="1")
+
+    if pic_data is not None:
+        audio.tags.add(
+            APIC(
+                encoding=3,
+                mime='image/jpeg',
+                type=3,
+                desc=u'Cover',
+                data=pic_data
+            )
+        )
+
     audio.save()
+
+
+def print_progress_bar(iteration, total, decimals=1, length=60, fill='#', nofill='-',
+                       prefix='Progress:', suffix='Complate'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + nofill * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end="")
+    sys.stdout.flush()
+    if iteration == total:
+        print()
 
 
 if __name__ == "__main__":
@@ -32,25 +57,46 @@ if __name__ == "__main__":
     if not dirPath.endswith("\\"):
         dirPath = dirPath + "\\"
 
-    for list in os.listdir(dirPath):
-        listPath = dirPath + list
+    total = 0
+    step = 0
+    print_progress_bar(0, total=1)
+
+    for listDir in os.listdir(dirPath):
+        listPath = dirPath + listDir
         if os.path.isdir(listPath):
             os.removedirs(listPath)
-        elif not (list.endswith(".mp3") or list.endswith(".wav")) or os.path.getsize(listPath) < 1024000 or list.find("_pv") != -1:
+        elif not (listDir.endswith(".mp3") or listDir.endswith(".wav")) or listDir.find("_pv") != -1 or os.path.getsize(listPath) < 1024000:
             os.remove(listPath)
+        else:
+            total += 1
+
+    total *= 3
+    total += 3
+    step += 1
+    print_progress_bar(step, total)
 
     jsonFile = open("./DeemoSongs.json", "r", encoding="utf-8")
     jsonObject = json.load(jsonFile)
     books = jsonObject["books"]
     songs = jsonObject["songs"]
 
+    step += 1
+    print_progress_bar(step, total)
+
     for i, v in enumerate(books):
         try:
-            os.mkdir(dirPath + v["name"].replace(":", " "))
+            os.mkdir(dirPath + ("%02d" % (i + 1)) + "." + v["name"].replace(":", " "))
         except FileExistsError:
             pass
 
+    cover = None
+    if os.path.isfile("./cover.jpg"):
+        cover = open("./cover.jpg", "rb").read()
+
     for file in os.listdir(dirPath):
+        step += 1
+        print_progress_bar(step, total)
+
         fullPath = dirPath + file
         if os.path.isdir(fullPath):
             continue
@@ -58,7 +104,15 @@ if __name__ == "__main__":
             WAVtoMP3(fullPath)
             os.remove(fullPath)
             fullPath = fullPath[:-3] + "mp3"
+
+        if not (file[:-4] in songs):
+            continue
+        step += 1
+        print_progress_bar(step, total)
         data = songs[file[:-4]]
-        book = books[data["book"]]
-        setMetaTag(fullPath, data["name"], data["artist"], book["name"], data["track"], book["track"])
-        shutil.move(fullPath, dirPath + book["name"].replace(":", " ") + "\\" + ("%02d" % data["track"]) + ". " + data["name"] + ".mp3")
+        book = list(filter(lambda x: x["id"] == data["book"], books))[0]
+
+        setMetaTag(fullPath, data["name"], data["artist"],("%02d" % (int(data["book"]) + 1)) + "." + book["name"], data["track"], book["track"], cover)
+        shutil.move(fullPath, dirPath + ("%02d" % (books.index(book) + 1)) + "." + book["name"].replace(":", " ") + "\\" + ("%02d" % data["track"]) + ". " + data["name"] + ".mp3")
+        step += 1
+        print_progress_bar(step, total)
